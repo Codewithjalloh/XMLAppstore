982eede3274246a60ecacee1ffbf65a11e34113c
info@codewithjalloh.com <supremejalloh>
2016-06-27 19:29:51 +0100

    connectionDidFinishLoading function added

--- XMLAppstore/ViewController.swift
+++ XMLAppstore/ViewController.swift
@@ -7,18 +7,53 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDataDelegate, NSXMLParserDelegate, UITableViewDelegate, UITableViewDataSource, ImagesOperationDelegate {
+    
+    @IBOutlet weak var myTable: UITableView!
+    var listData: NSMutableData!
+    var currentString: NSMutableString!
+    var shouldParse:Bool!
+    var apps: NSMutableArray!
+    var currentApp:AppInfo!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var queue = NSOperationQueue()
+        let urlRequest = NSURLRequest(URL: NSURL(string: "http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=75/xml")!)
+        
+        NSURLConnection(request: urlRequest, delegate: self)
+        currentString = NSMutableString(string: "")
+        apps = NSMutableArray()
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        listData = NSMutableData()
+        print("did receive response")
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        listData.appendData(data)
+        print("did receive data")
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        print("did finish loading")
+        let parser = NSXMLParser(data: listData)
+        parser.delegate = self
+        parser.parse()
+        myTable.reloadData()
+        
+        let queue = NSOperationQueue()
+        for it in apps {
+            let imagesOperation = imagesOperation()
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
