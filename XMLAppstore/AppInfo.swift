7bf2e74d4f08095bbee9ea799449770b709e9e14
info@codewithjalloh.com <supremejalloh>
2016-06-27 18:34:38 +0100

    data model added

---
+++ XMLAppstore/AppInfo.swift
@@ -0,0 +1,17 @@
+//
+//  AppInfo.swift
+//  XMLAppstore
+//
+//  Created by Jalloh on 27/06/2016.
+//  Copyright © 2016 CodeWithJalloh. All rights reserved.
+//
+
+import UIKit
+
+class AppInfo: NSObject {
+    var urlApp: String!
+    var name: String!
+    var urlImage: String!
+    var index: Int!
+    var image: UIImage!
+}
